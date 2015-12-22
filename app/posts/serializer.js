23e34b4b7d12ba78fea5b0178939ff6a93b12f15
Craig Bilner <craig.bilner@gmail.com>
2015-12-22 22:32:40 +0000

    Wire in API
    Remove cruft
    Add glue between template and API
    Upgrade to canary

--- app/posts/serializer.js
+++ app/posts/serializer.js
@@ -1,4 +1,20 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
+  normalizeFindAllResponse(store, primaryModelClass, { objects }, id, requestType) {
+    const response = {
+      posts: objects.map(post => ({
+        type: 'posts',
+        id: post._id,
+        content: post.content,
+        create: new Date(post.created),
+        modified: new Date(post.modified),
+        slug: post.slug,
+        title: post.title,
+        tags: post.metafields[0].value.split('|')
+      }))
+    };
+
+    return this._super(store, primaryModelClass, response, id, requestType);
+  }
 });
