39187a684d071ec142d2a25ef57f9258914233da
Craig Bilner <craig.bilner@gmail.com>
2015-12-26 00:59:27 +0000

    Redeploy

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    vendorFiles: {
+    'jquery.js': {
+        development: 'bower_components/jquery/dist/jquery.js',
+        production: false
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
