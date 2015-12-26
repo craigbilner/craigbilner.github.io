3f5131d07cfc0494c5acc26a92c60c3babad0bcf
Craig Bilner <craig.bilner@gmail.com>
2015-12-26 18:51:19 +0000

    Swap AJAX for fetch

--- app/post/adapter.js
+++ app/post/adapter.js
@@ -1,9 +1,23 @@
 import DS from 'ember-data';
 
+const URL = 'https://api.cosmicjs.com/v1/blog-cb/objects';
+
+function handleFetch (response) {
+  if (response.ok) {
+    return response.json().then(jsonResponse => jsonResponse);
+  } else {
+    console.error('Network response was not ok.');
+  }
+}
+
+function handleError (error) {
+  console.error('There has been a problem with your fetch operation', error);
+}
+
 export default DS.JSONAPIAdapter.extend({
-  host: 'https://api.cosmicjs.com',
-  namespace: 'v1/blog-cb',
-  pathForType: () => {
-    return 'objects';
+  ajax() {
+    return fetch(URL)
+      .then(handleFetch)
+      .catch(handleError);
   }
 });
