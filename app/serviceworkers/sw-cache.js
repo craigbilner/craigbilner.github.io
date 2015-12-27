cd073bcc201ba6928f4a61b8937d1b57e0e9044a
Craig Bilner <craig.bilner@gmail.com>
2015-12-27 11:44:11 +0000

    Add logging

--- app/serviceworkers/sw-cache.js
+++ app/serviceworkers/sw-cache.js
@@ -1,5 +1,6 @@
 function cacheOpen (event, response) {
   return function (cache) {
+    console.info('caching', event.request.url);
     cache.put(event.request, response.clone());
     return response;
   }
@@ -7,6 +8,7 @@ function cacheOpen (event, response) {
 
 function fetchSuccess (event) {
   return function (response) {
+    console.info('fetch success', event.request.url);
     return caches
       .open('v1')
       .then(cacheOpen(event, response));
@@ -15,6 +17,7 @@ function fetchSuccess (event) {
 
 function noCache (event) {
   return function () {
+    console.info('no cache', event.request.url);
     return fetch(event.request)
       .then(fetchSuccess(event));
   }
@@ -25,6 +28,7 @@ function noCacheFail () {
 }
 
 this.addEventListener('fetch', function (event) {
+  console.info('fetching', event.request.url);
   event.respondWith(
     caches
       .match(event.request)
