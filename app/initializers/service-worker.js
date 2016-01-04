8f2635e5aa9cdebf4a3ddb9afcc24d28c8fde1eb
Craig Bilner <craig.bilner@gmail.com>
2016-01-04 23:33:20 +0000

    Revert uninstall

--- app/initializers/service-worker.js
+++ app/initializers/service-worker.js
@@ -1,16 +1,9 @@
 export function initialize () {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-      for (let registration of registrations) {
-        console.info('unregistering', registration);
-        registration.unregister();
-      }
-      console.info('registering', './sw.js');
-      navigator.serviceWorker.register('./sw.js')
-        .catch(error => {
-          console.error(`Error registering service worker:${error}`);
-        });
-    });
+    navigator.serviceWorker.register('./sw.js')
+      .catch(function (error) {
+        console.error('Error registering service worker:' + error);
+      });
   } else {
     console.warn('service worker not supported');
   }
