7b9233c2f67798bac905f395f5232cd90b0b3e8f
Craig Bilner <craig.bilner@gmail.com>
2016-01-04 23:02:24 +0000

    Uninstall old service worker

--- app/initializers/service-worker.js
+++ app/initializers/service-worker.js
@@ -1,9 +1,14 @@
 export function initialize () {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
-      .catch(function (error) {
-        console.error('Error registering service worker:' + error);
-      });
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+      navigator.serviceWorker.register('./sw.js')
+        .catch(error => {
+          console.error(`Error registering service worker:${error}`);
+        });
+    });
   } else {
     console.warn('service worker not supported');
   }
