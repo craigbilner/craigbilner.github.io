0781cb763f9abdb2d7bede91bbe28e1403918f88
Craig Bilner <craig.bilner@gmail.com>
2016-01-28 23:10:16 +0000

    Remove redundant logic

--- app/initializers/service-worker.js
+++ app/initializers/service-worker.js
@@ -1,24 +1,12 @@
 export function initialize() {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(function (registrations) {
-      const activeWorkers = [];
-      for (let registration of registrations) {
-        activeWorkers.push(registration.active && registration.active.scriptURL);
-      }
-      const hasWorker = activeWorkers.some(worker => ~worker.indexOf('sw-cache.js'));
-      if (!hasWorker) {
-        console.info('worker does not exist');
-        navigator.serviceWorker.register('/sw-cache.js')
-          .then(() => {
-            console.info('worker registered');
-          })
-          .catch(error => {
-            console.error(`error registering service worker:${error}`);
-          });
-      } else {
-        console.info('worker already registered');
-      }
-    });
+    navigator.serviceWorker.register('/sw-cache.js')
+      .then(() => {
+        console.info('worker registered');
+      })
+      .catch(error => {
+        console.error(`error registering service worker:${error}`);
+      });
   } else {
     console.warn('service worker not supported');
   }
